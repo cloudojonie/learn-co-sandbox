851cefb00f5d00b353154791e97b8ba89e2c2254
Jonathan Erquinigo <fighter282@gmail.com>
2018-06-26 03:10:28 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,13 @@
+var name ="joe";
+var height = "74";
+
+module.exports = {name, height}
+
+if (name === "Susan"){
+  
+  console.log("The name is correct");
+} else{
+  
+  console.log("Expected the name to be Susan");
+  
+}
---
+++ test.js
@@ -0,0 +1,17 @@
+var index = require("./index.js")
+name = "Joe";
+height = "74";
+
+if (name === "Joe"){
+console.log("the name is correct");
+
+} else {
+  
+  console.log("this is wrong");
+}
+
+if (height === "74"){
+  console.log("the Height is correct");
+  
+  
+}
